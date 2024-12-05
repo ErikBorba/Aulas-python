8e625748c26d5caedb2ec82973519fa92529e409
ErikBorba <erikborba.p30@gmail.com>
2024-12-05 11:24:03 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,9 @@
+print ('Bom dia')
+nome_usuario = 'Erik Borba' # tipo >str string
+idade_usuario = 19 # tipo > int = integer
+altura_usuario = 1.85 # tipo > ] float = flutuante
+maior_idade = True # tipo > bool = boolean
+print ('Seja Bem Vindo', nome_usuario)
+print ('sua idade', idade_usuario,'anos')
+print('voce tem',altura_usuario ,'metros') 
+print ('voce é maior de idade?',maior_idade)
